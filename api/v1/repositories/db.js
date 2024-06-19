22abb80835af336a0101928363cfe2380ebd1b64
WanCarvalho <wandersoncarvalho.1501@gmail.com>
2024-06-19 16:03:19 -0300

    feature: conexão com banco de dados e criação da model para entidade Hotel

---
+++ api/v1/repositories/db.js
@@ -0,0 +1,15 @@
+import Sequelize from "sequelize";
+
+// estabelece conexão com banco de dados através da sequelize ORM
+const sequelize = new Sequelize(
+    process.env.POSTGRES_DB_NAME,
+    process.env.POSTGRES_DB_USERNAME,
+    process.env.POSTGRES_DB_PASSWORD, {
+        
+    dialect: 'postgres',
+    define: {
+        timestamp: false
+    }
+});
+
+export default sequelize;
--- api/v1/repositories/hotels.repository.js
+++ api/v1/repositories/hotels.repository.js
@@ -1,12 +1,52 @@
-async function getHotels(){}
+import Hotel from "../models/hotels.model.js";
 
-async function getHotel(){}
+async function getHotels(){
+    try {
+        return await Hotel.findAll();
+    } catch(err) {
+        throw err;
+    }
+}
 
-async function createHotel(){}
+async function getHotel(id){
+    try {
+        return await Hotel.findByPk(id);
+    } catch(err) {
+        throw err;
+    }
+}
 
-async function deleteHotel(){}
+async function createHotel(hotel){
+    try {
+        return await Hotel.create(hotel);
+    } catch(err) {
+        throw err;
+    }
+}
 
-async function updateHotel(){}
+async function deleteHotel(id){
+    try {
+        await Hotel.destroy({
+            where: {
+                hotelId: id,
+            }
+        });
+    } catch(err) {
+        throw err;
+    }
+}
+
+async function updateHotel(hotel){
+    try {
+       await Hotel.update(hotel, {
+        where: {
+            hotelId: hotel.hotelId,
+        }
+       });
+    } catch(err) {
+        throw err;
+    }
+}
 
 export default {
     getHotels,
