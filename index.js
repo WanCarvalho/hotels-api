a14d27b96e29c9a6ef9b46f41b03a63826589b74
WanCarvalho <wandersoncarvalho.1501@gmail.com>
2024-06-20 06:46:27 -0300

    config: configurando logger para registrar requisições

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 import "./env.js";
 import express from "express";
 import cors from "cors";
+import logger from "./api/config/logger.js";
 import RouterV1 from "./api/v1/router.js";
 import RouterV2 from "./api/v2/router.js";
+
+global.logger = logger;
 
 const app = express();
 app.use(express.json());
@@ -14,5 +17,12 @@ app.use(function (req, res, next) {
 
 app.use("/api/v1", RouterV1);
 app.use("/api/v2", RouterV2);
+
+// middleware para tratamento de erros precisa ser colocado como último da cadeia de requisições
+app.use((err, req, res, next) => {
+    res
+        .status(err.statusCode || 500)
+        .send({ error: err.message || "Something went wrong" });
+});
 
 app.listen(3000, () => console.log("Hotels API started."));
